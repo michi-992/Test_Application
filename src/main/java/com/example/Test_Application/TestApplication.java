3ce95c7e53d336a5114ad035fcd59acff75f3c97
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-21 14:11:20 +0200

    Initial commit

---
+++ src/main/java/com/example/Test_Application/TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
