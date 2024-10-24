fbdd19b282860e5deb758de9d5dbaab78dc41674
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-24 13:17:43 +0200

    defined data model

--- src/main/java/com/example/Test_Application/model/Tutorial.java
+++ src/main/java/com/example/Test_Application/model/Tutorial.java
@@ -1,4 +1,19 @@
 package com.example.Test_Application.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tutorials")
 public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
 }
