53fb1c0afed83591a0a56374f4230ace0bc991c6
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 19:13:15 +0200

    created search-item test directory

--- src/test/java/com/example/Test_Application/SearchItem/SearchItemsControllerTest.java
+++ src/test/java/com/example/Test_Application/SearchItem/SearchItemsControllerTest.java
@@ -1,6 +1,5 @@
-package com.example.Test_Application;
+package com.example.Test_Application.SearchItem;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,20 +8,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.example.Test_Application.controller.SearchItemController;
 import com.example.Test_Application.model.SearchItem;
-import com.example.Test_Application.repository.SearchItemService;
+import com.example.Test_Application.service.SearchItemService;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 import java.util.List;
