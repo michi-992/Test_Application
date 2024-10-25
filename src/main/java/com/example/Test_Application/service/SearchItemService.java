eb0b1badaa582d67789d2eeccd395d88a845dfeb
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 19:14:57 +0200

    created SearchItemService

---
+++ src/main/java/com/example/Test_Application/service/SearchItemService.java
@@ -0,0 +1,22 @@
+package com.example.Test_Application.service;
+
+import com.example.Test_Application.model.SearchItem;
+import com.example.Test_Application.repository.SearchItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SearchItemService {
+    private SearchItemRepository searchItemRepo;
+
+    public SearchItemService(SearchItemRepository searchItemRepository) {
+        this.searchItemRepo = searchItemRepository;
+    }
+
+    public List<SearchItem> getSearchItems() throws Exception {
+        Optional<List<SearchItem>> searchitems = searchItemRepo.getSearchItems();
+        return searchitems.get();
+    }
+}
