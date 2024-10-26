b86d1d61308042f3a4a24ba579e4c039fd7cbcf0
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-26 15:05:48 +0200

    created unit test for exception case

--- src/test/java/com/example/Test_Application/SearchItem/SearchItemsServiceTest.java
+++ src/test/java/com/example/Test_Application/SearchItem/SearchItemsServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.Test_Application.SearchItem;
 
+import com.example.Test_Application.exceptions.SearchItemListNotFoundException;
 import com.example.Test_Application.model.SearchItem;
 import com.example.Test_Application.repository.SearchItemRepository;
 import com.example.Test_Application.service.SearchItemService;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +38,19 @@ public class SearchItemsServiceTest {
         List<SearchItem> foundSearchItems = searchItemRepo.findAll();
 
         assertThat(foundSearchItems).isEqualTo(searchItemsList);
+
+        verify(searchItemRepo).findAll();
+    }
+
+    @Test
+    public void getSearchItemsNotFound() {
+        SearchItemService searchItemService = new SearchItemService(searchItemRepo);
+
+        when(searchItemRepo.findAll()).thenReturn(Collections.emptyList());
+
+        assertThrows(SearchItemListNotFoundException.class, () -> {
+            searchItemService.getSearchItems();
+        });
 
         verify(searchItemRepo).findAll();
     }
