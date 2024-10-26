e19fb24f3070907341befe4d96d5760b0f52c79d
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-26 13:21:35 +0200

    added @CrossOrigni for SearchItemController

--- src/main/java/com/example/Test_Application/controller/SearchItemController.java
+++ src/main/java/com/example/Test_Application/controller/SearchItemController.java
@@ -2,12 +2,14 @@ package com.example.Test_Application.controller;
 
 import com.example.Test_Application.model.SearchItem;
 import com.example.Test_Application.service.SearchItemService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class SearchItemController {
     private SearchItemService searchItemService;
 
