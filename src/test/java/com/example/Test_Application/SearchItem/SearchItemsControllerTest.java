a4a36264b40d2f2773e92ae6f73644c944a05866
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-26 16:01:15 +0200

    added contentType for GET

--- src/test/java/com/example/Test_Application/SearchItem/SearchItemsControllerTest.java
+++ src/test/java/com/example/Test_Application/SearchItem/SearchItemsControllerTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -42,7 +43,7 @@ public class SearchItemsControllerTest {
         given(searchItemService.getSearchItems()).willReturn(mockSearchItems);
 
         String expectedJson = "[{\"id\":1,\"searchTerm\":\"testTerm1\"},{\"id\":2,\"searchTerm\":\"testTerm2\"}]";
-        ResultActions result = mockMvc.perform(get("/searchItems"))
+        ResultActions result = mockMvc.perform(get("/searchItems").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedJson));
 
