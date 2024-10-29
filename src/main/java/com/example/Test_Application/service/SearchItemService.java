3374db9aba29c49b861bcb52eb5e15520d83bf1d
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-29 18:11:19 +0100

    split searchItemService into interface & impl.

--- src/main/java/com/example/Test_Application/service/SearchItemService.java
+++ src/main/java/com/example/Test_Application/service/SearchItemService.java
@@ -2,30 +2,10 @@ package com.example.Test_Application.service;
 
 import com.example.Test_Application.exceptions.SearchItemListNotFoundException;
 import com.example.Test_Application.model.SearchItem;
-import com.example.Test_Application.repository.SearchItemRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class SearchItemService {
-    private SearchItemRepository searchItemRepo;
-
-    public SearchItemService(SearchItemRepository searchItemRepository) {
-        this.searchItemRepo = searchItemRepository;
-    }
-
-    public List<SearchItem> getSearchItems() throws SearchItemListNotFoundException {
-        List<SearchItem> searchItems = searchItemRepo.findAll();
-
-        if(searchItems.isEmpty()) {
-            throw new SearchItemListNotFoundException();
-        }
-        return searchItems;
-    }
-
-    public SearchItem addSearchItem(SearchItem searchItem) {
-        return searchItemRepo.save(searchItem);
-    }
+public interface SearchItemService {
+    List<SearchItem> getSearchItems() throws SearchItemListNotFoundException;
+    SearchItem addSearchItem(SearchItem searchItem);
 }
---
+++ src/main/java/com/example/Test_Application/service/SearchItemServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.Test_Application.service;
+
+import com.example.Test_Application.exceptions.SearchItemListNotFoundException;
+import com.example.Test_Application.model.SearchItem;
+import com.example.Test_Application.repository.SearchItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchItemServiceImpl implements SearchItemService {
+    private SearchItemRepository searchItemRepo;
+    public SearchItemServiceImpl(SearchItemRepository searchItemRepo) {
+        this.searchItemRepo = searchItemRepo;
+    }
+
+    @Override
+    public List<SearchItem> getSearchItems() throws SearchItemListNotFoundException {
+        List<SearchItem> searchItems = searchItemRepo.findAll();
+        if (searchItems.isEmpty()) {
+            throw new SearchItemListNotFoundException();
+        }
+        return searchItems;
+    }
+
+    @Override
+    public SearchItem addSearchItem(SearchItem searchItem) {
+        return searchItemRepo.save(searchItem);
+    }
+}
