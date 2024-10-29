174fe0cd0da86f72f64890f30c71daf6d076e629
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-29 14:32:35 +0100

    Added comment about ServletInitializer

--- src/main/java/com/example/Test_Application/ServletInitializer.java
+++ src/main/java/com/example/Test_Application/ServletInitializer.java
@@ -3,6 +3,12 @@ package com.example.Test_Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+/*
+Diese Klasse initialisiert die Spring Boot-Anwendung für die Bereitstellung als WAR-Datei
+in einem traditionellen Java EE-Servlet-Container (Tomcat).
+Sie ist nur erforderlich, wenn die Anwendung als WAR-Datei bereitgestellt wird.
+*/
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
